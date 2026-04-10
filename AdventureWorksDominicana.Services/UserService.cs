5800f252c1413c1c64964f1227ad52014dc45d2d
Raydelis Hilario <raydelishilariomendez@gmail.com>
2026-04-09 22:01:17 -0400

    Agrega manejo de cambio de rol

--- AdventureWorksDominicana.Services/UserService.cs
+++ AdventureWorksDominicana.Services/UserService.cs
@@ -14,7 +14,7 @@ public class UserService(IDbContextFactory<Contexto> DbFactory) : IService<AspNe
     public async Task<AspNetUser?> Buscar(string id)
     {
         await using var contexto = await DbFactory.CreateDbContextAsync();
-        return await contexto.AspNetUsers.FirstOrDefaultAsync(p => p.Id == id);
+        return await contexto.AspNetUsers.Include(x => x.Roles).FirstOrDefaultAsync(p => p.Id == id);
     }
 
     public async Task<bool> Eliminar(string id)
@@ -24,7 +24,7 @@ public class UserService(IDbContextFactory<Contexto> DbFactory) : IService<AspNe
 
         if (user == null) return false;
 
-        contexto.AspNetUsers.Eliminado = true;
+        //contexto.AspNetUsers.Eliminado = true;
         return await contexto.SaveChangesAsync() > 0;
     }
 
@@ -37,7 +37,30 @@ public class UserService(IDbContextFactory<Contexto> DbFactory) : IService<AspNe
     public async Task<bool> Guardar(AspNetUser entidad)
     {
         await using var contexto = await DbFactory.CreateDbContextAsync();
-        contexto.AspNetUsers.Update(entidad);
+
+        var usuarioDb = await contexto.AspNetUsers
+            .Include(u => u.Roles)
+            .FirstOrDefaultAsync(u => u.Id == entidad.Id);
+
+        if (usuarioDb == null) return false;
+
+        usuarioDb.Email = entidad.Email;
+        usuarioDb.NormalizedEmail = entidad.Email.ToUpper();
+        usuarioDb.LockoutEnabled = entidad.LockoutEnabled;
+        //usuarioDb.Eliminado = entidad.Eliminado;
+        
+        usuarioDb.Roles.Clear();
+
+        var nuevoRolId = entidad.Roles.FirstOrDefault()?.Id;
+
+        if (!string.IsNullOrEmpty(nuevoRolId))
+        {
+            var rolDb = await contexto.AspNetRoles.FindAsync(nuevoRolId);
+            if (rolDb != null)
+            {
+                usuarioDb.Roles.Add(rolDb);
+            }
+        }
         return await contexto.SaveChangesAsync() > 0;
     }
     public async Task<List<AspNetRole>> GetListRoles(Expression<Func<AspNetRole, bool>> criterio)
